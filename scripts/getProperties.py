494cfc1a838be8b409dc0393fa13f99c501630b9
Joshua Saltzberg <joshua.saltzberg@einsteinmed.edu>
2026-05-17 21:01:21 -0400

    Update getProperties script to match new method

--- scripts/getProperties.py
+++ scripts/getProperties.py
@@ -6,7 +6,8 @@ import pandas as pd
 from typing import cast
 from pathlib import Path
 
-from rates2chip.parsing import getRegions, getCoverage
+from rates2chip.pro import getWindows
+from rates2chip.parsing import getCoverage
 from rates2chip.utilities import import_pandas, export_pandas, subset_pandas
 
 
@@ -16,7 +17,13 @@ if __name__ == '__main__':
 	parser.add_argument('--pro_path',required=False,help='path to PROseq dataframe')
 	parser.add_argument('--bigwig',nargs='+',default=[],help='path to bigwig files')
 	parser.add_argument('--filter',type=str,default='converged,replicated,valid_dependencies,~upper,~lower',help='coma separated list of boolean columns to filter by. use ~ to filter by negation of column')
-	parser.add_argument('--method',default='',help='window method to use')
+	parser.add_argument('--method',default='',help='window method to use ()')
+	parser.add_argument('--tss_offset',default=500,help='distance from tss to mask')
+	parser.add_argument('--cps_offset',default=0,help='distance from CPS to mask')
+	parser.add_argument('--min_len',default=50,help='minimum window length (nt)')
+	parser.add_argument('--min_time',default=5,help='minimum window time (min)')
+	parser.add_argument('--min_pro',default=1,help='minimum window polymerase count')
+	parser.add_argument('--max_windows',required=False,help='maximum number of windows per gene')
 	parser.add_argument('--out',help='output path')
 	parser.add_argument('--debug',action='store_true')
 	args = parser.parse_args()
@@ -33,10 +40,14 @@ if __name__ == '__main__':
 
 	# Get gene properties
 	if args.rates:
-		rate_df = import_pandas(Path(args.rates))
-		if args.pro_path: pro_df = import_pandas(Path(args.pro_path))
-		else: pro_df = None
-		out_df = getRegions(rate_df,pro_df,args.method,cast(str,args.filter).split(','))
+		rate_df = subset_pandas(import_pandas(Path(args.rates)),cast(str,args.filter).split(','))
+		if args.pro_path: 
+			rate_df = rate_df[rate_df['type'] == 'rate']
+			pro_df = import_pandas(Path(args.pro_path))
+			getWindows(rate_df,pro_df,args.tss_offset,args.cps_offset,args.min_len,args.min_time,args.min_pro,args.max_windows)
+		else: 
+			rate_df = rate_df[rate_df['type'] == 'elongation']
+	
 	else: out_df = import_pandas(out_path)
 
 	# Get Bigwig
